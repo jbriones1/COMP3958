4797914ab7fc19eccff57b8d1928e8f400448cdb
Jon Andre Briones <jbriones1@my.bcit.ca>
2020-11-08 03:24:16 -0800

    About to change everything to sort by frequency

---
+++ Asn1/leftist_heap.ml
@@ -0,0 +1,85 @@
+module type OrderedType = sig
+  type t
+  val compare: t -> t -> int
+end
+
+module type S = sig
+  type elt
+  type t
+  val empty : t
+  val merge : t -> t -> t
+  val insert : t -> elt -> t
+  val heap_of_list : elt list -> t
+  val get_min : t -> elt option
+  val delete_min : t -> t
+  val heap_sort : elt list -> elt list
+end
+
+module Make(Ord: OrderedType) = struct
+  type elt = Ord.t
+  type t = E | T of int * elt * t * t
+
+  let rank = function
+    | E -> 0
+    | T (r, _, _, _) -> r
+
+  let empty = E
+
+  let rec merge t1 t2 =
+    match t1, t2 with
+    | E, t | t, E -> t
+    | T (_, x1, _, _), T(_, x2, _, _)  when Ord.compare x2 x1 < 0 ->
+      merge t2 t1
+    | T (_, x, l, r), t ->
+      let r' = merge r t in
+      if rank l >= rank r' then T (1 + rank r', x, l, r')
+      else T (1 + rank l, x, r', l)
+
+  let insert t x = merge t (T (1, x, E, E))
+
+  let heap_of_list l =
+    let open Base in
+    List.fold_left ~init:E ~f:(fun t x -> insert t x) l
+
+  let get_min = function
+    | E -> None
+    | T (_, x, _, _) -> Some x
+
+  let delete_min = function
+    | E -> failwith "delete_min: empty tree"
+    | T (_, _, l, r) -> merge l r
+
+  (**
+   * Question 2
+   *
+   * Sorts a list using the heap, in ascending order.
+   * 
+   * lst: the list being sorted
+   * h_list: min_heap of the list being sorted
+   * 
+   * aux: the recursive function that takes the heap and creates a list from it
+   *
+   * lst': the sorted list of objects
+   * heap: the heap being sorted from
+   *)
+  let heap_sort lst =
+    let h_list = heap_of_list lst in
+      let rec aux lst' heap =
+        match heap with
+        | E -> List.rev lst'
+        | T (_, x, _, _) ->  aux (x::lst') (delete_min heap) in
+    aux [] h_list
+end
+
+module M = Make(String)
+
+open M
+
+  let print_list l =
+    List.iter (fun x -> Printf.printf "%s " x) l
+
+let lst = ["d";"b";"a";"c"]
+let () = Printf.printf "Sorting: "; print_list lst; Printf.printf "\n"
+let heap_sort_test = heap_sort lst
+let () = print_list heap_sort_test; Printf.printf "\n"
+
---
+++ Asn1/main.ml
@@ -0,0 +1,122 @@
+module type OrderedType = sig
+  type t
+  val compare: t -> t -> int
+end
+
+module type S = sig
+  type elt
+  type t
+  val empty : t
+  val merge : t -> t -> t
+  val insert : t -> elt * int -> t
+  val heap_of_list : elt list -> t
+  val get_min : t -> elt option
+  val delete_min : t -> t
+  val heap_sort : elt list -> elt list
+end
+
+module Make(Ord: OrderedType) = struct
+  type elt = Ord.t
+  type t = E | T of int * elt * t * t
+
+  let rank = function
+    | E -> 0
+    | T (r, _, _, _) -> r
+
+  let empty = E
+
+  let rec merge t1 t2 =
+    match t1, t2 with
+    | E, t | t, E -> t
+    | T (_, x1, _, _), T(_, x2, _, _) when Ord.compare x2 x1 < 0 ->
+      merge t2 t1
+    | T (_, x, l, r), t ->
+      let r' = merge r t in
+      if rank l >= rank r' then T (1 + rank r', x, l, r')
+      else T (1 + rank l, x, r', l)
+
+  let insert t x = merge t (T (1, x, E, E))
+
+  let heap_of_list l =
+    let open Base in
+    List.fold_left ~init:E ~f:(fun t x -> insert t x) l
+
+  let get_min = function
+    | E -> None
+    | T (_, x, _, _) -> Some x
+
+  let delete_min = function
+    | E -> failwith "delete_min: empty tree"
+    | T (_, _, l, r) -> merge l r
+
+  let heap_sort lst =
+    let h_list = heap_of_list lst in
+    let rec aux lst' heap =
+      match heap with
+      | E -> List.rev lst'
+      | T (_, x, _, _) ->  aux (x::lst') (delete_min heap) in
+    aux [] h_list
+end
+
+module Frequency_tree = struct
+  type t = E | T of (string * int) * t * t
+
+  let compare t1 t2 =
+    match t1, t2 with
+    | E, E -> 0
+    | t, E | E, t -> 0
+    | (T((_, x1), _, _)), (T((_, x2), _, _)) -> Base.Int.compare x1 x2
+end
+
+module FT = Make(Frequency_tree)
+
+(* 
+ * Uses the fold_left function to count the frequency of unique strings
+ *
+ * lst: the list being processed
+ * tup: tuple being checked being checked
+ * n: the character string being checked
+ * acc: accumulator that builds up a list of (string * int)
+*)
+let frequencies_fold lst =
+  let open Base in
+  let increment_if_match tup n =
+    match tup with
+    | (k, v) -> 
+      if String.equal k n 
+      then [(k, v+1)] 
+      else [(n,1);(k,v)] in 
+  let increment acc n =
+    match acc with
+    | [] -> (n, 1)::acc
+    | h::t -> (increment_if_match h n) @ t; in 
+  List.sort lst ~compare: String.compare 
+  |> List.fold_left ~init:[] ~f:increment |> List.rev 
+
+let calc_frequencies str =
+
+  (* Break the string into a list of string made of its characters *)
+  let str_list = 
+    String.to_seq str |> List.of_seq |> List.sort Char.compare 
+    |> List.map (String.make 1) in
+
+  frequencies_fold str_list
+
+let encode_text text codes =
+
+  let list_of_text = 
+    String.to_seq text |> List.of_seq |> List.map (String.make 1) in
+
+  Base.List.fold_left list_of_text ~init:"" 
+    ~f:(fun acc x ->  acc ^ (List.assoc x codes))
+
+let text = 
+  "A_DEAD_DAD_CEDED_A_BAD_BABE_A_BEADED_ABACA_BED"
+let huffman_codes = 
+  [("_","00");("A","10");("B","1111");("C","1110");("D","01");("E","110")]
+
+
+
+(* MAIN FUNCTION *)
+let a = encode_text text huffman_codes
+let () = Printf.printf "%s (%d)\n" a (String.length a)
