2579c8f8dc3470e2320011f5ed6708f1b23919f2
Jon Andre Briones <jbriones1@my.bcit.ca>
2020-11-26 13:44:20 -0800

    Completed base functions

--- Asn2/my_trie.ml
+++ Asn2/my_trie.ml
@@ -22,28 +22,41 @@ let charlist_of_string str =
 (* 
  * Takes a string and adds it to the list of characters. 
  *)
-let add str t =
+let add str tr =
   let char_list = charlist_of_string str in
-  let rec ins ch tr =
-    match ch, tr with
+  let rec add' ch tr' =
+    match ch, tr' with
       [], N (_, m) -> N (true, m)
-    | h::t, N (b, m) ->
-      if M.mem h m then N (b, M.add h (ins t (M.find h m)) m)
-      else N (b, M.add h (ins t empty) m)
-  in ins char_list t
+    | h::t, N (b, m) when M.mem h m -> N (b, M.add h (add' t (M.find h m)) m)
+    | h::t, N (b, m) -> N (b, M.add h (add' t empty) m)
+  in add' char_list tr
 
-let find str t = 
+let find str tr = 
   let char_list = charlist_of_string str in
-  let rec traverse ch tr =
-    match ch, tr with
+  let rec find' ch tr' =
+    match ch, tr' with
       [], N (b, _) -> b
     | h::t, N (b, m) -> 
-      if M.mem h m then traverse t (M.find h m)
+      if M.mem h m then find' t (M.find h m)
       else false
-  in traverse char_list t
+  in find' char_list tr
 
-let count tr = 
-  let rec count' tr' acc =
-    let chk_word b = if b then 1 else 0 in
-    M.fold (fun _ t acc -> (count' t (acc + (chk_word b)))) m acc
+let count tr =
+  let chk_word t = if t then 1 else 0 in
+  let rec count' (N (b, l)) acc =
+    M.fold (fun _ (N (b', _) as tr') acc -> (count' tr' (acc + (chk_word b')))) l acc
   in count' tr 0
+
+let rec traverse (N(e, m)) f =
+  M.iter (fun k (N(e', m')) -> f k e'; traverse (N(e', m')) f) m
+
+let print tr =
+  let open Stdio in
+  traverse tr (
+    fun k e ->
+      if e then printf "%c\n" k
+      else printf "%c" k
+  )
+
+let of_list lst = 
+  Base.List.fold_left ~init:empty ~f:(fun acc x -> add x acc) lst
